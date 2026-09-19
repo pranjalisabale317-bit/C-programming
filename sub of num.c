2c86c4a7ba9b59ae63128a2fecd30229f2c93a7d
pranjalisabale317-bit <pranjalisabale317@gmail.com>
2026-09-19 13:28:45 +0530

    subtraction program in num.c

---
+++ sub of num.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,sub;
+    printf("\n Enter value a,b:");
+    scanf("\n%d\n%d",&a,&b);
+    sub=a-b;
+    printf("\n substraction of a and b=%d",sub);
+        return 0;
+}
+    
