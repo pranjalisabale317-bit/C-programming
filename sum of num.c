1b23d7cd0ee0656e720d8cac9527140342410e6f
pranjalisabale317-bit <pranjalisabale317@gmail.com>
2026-09-19 13:21:28 +0530

    program to calculate sum of two numbers

---
+++ sum of num.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    printf("\n Enter value a,b:");
+    scanf("\n%d\n%d",&a,&b);
+    sum =a+b;
+    printf("\n sum of a and b=%d",sum);
+        return 0;
+}
+    
