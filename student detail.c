3ab2f1f1faa9d8e533360e5271c45c23a621716a
pranjalisabale317-bit <pranjalisabale317@gmail.com>
2026-09-19 17:30:26 +0530

    student detail program in C

---
+++ student detail.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int rn=101;
+    float marks=89.21;
+    char n1='R',n2='o',n3='s',n4='y';
+    printf("************Student detail***********");
+    printf("\nRoll no*:%d",rn);
+    printf("\nName:%c%c%c%c",n1,n2,n3,n4);
+    printf("\nMarks:%f",marks);
+    return 0;
+}
+    
+    
