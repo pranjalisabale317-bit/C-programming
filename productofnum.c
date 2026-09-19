5da663cfabcc5d2b90dd2a4beda0fb06bb56e8e1
pranjalisabale317-bit <pranjalisabale317@gmail.com>
2026-09-19 13:24:33 +0530

    program to calculate product of two numbers

---
+++ productofnum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,product;
+    printf("\n Enter value a,b:");
+    scanf("\n%d\n%d",&a,&b);
+    product =a*b;
+    printf("\n product of a and b=%d",product);
+        return 0;
+}
+    
