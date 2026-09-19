32c12e476264f985a13e87ccbe0d2cba14c3d763
pranjalisabale317-bit <pranjalisabale317@gmail.com>
2026-09-19 13:26:28 +0530

    C program for division of two numbers

---
+++ div of num.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,div;
+    printf("\n Enter value a,b:");
+    scanf("\n%d\n%d",&a,&b);
+    div=a/b;
+    printf("\n division of a and b=%d",div);
+        return 0;
+}
+    
