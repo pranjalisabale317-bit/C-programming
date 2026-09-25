74adcfad02e4ce4f1f666872a880555f0b06279f
pranjali sabale <pranjalisabale317@gmail.com>
2026-09-25 22:51:45 +0530

    format_specifiers.c for character printing examples

---
+++ format_specifiers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char a='A';
+    char b[6]="Hello";
+    char c[50]="String is collection of multicharecter"; 
+    printf("\n Single charecter=%c:",a);
+    printf("\n multicharecter=%s",b);
+    printf("\n multicharecter=%s",c);
+    return 0;
+    
+}
